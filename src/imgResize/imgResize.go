821556ecac33ecb726c4279eb887ce5f8ba9930d
Igor B <84845632+Ig0rFB@users.noreply.github.com>
2024-05-03 14:55:26 +0100

    test

--- src/imgResize/imgResize.go
+++ src/imgResize/imgResize.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"image"
+	_ "image/jpeg"
 	"log"
 	"os"
 )
-
-func main() {
-	file, err := os.Open("arriRed.jpg")
-
-	if err != nil {
-		log.Fatalln(err)
+	
+func main(){
+	file, err:= os.Open("arriRef.jpg")
+	if err != nil{
+		log.Fatalln("File not found.")
 	}
+defer file.Close()
 
+src, _, err := image.Decode(file)
+if err != nil {
+	log.Fatalln(err)
+}
+
+bounds := src.Bounds()
+log.Print("Image Bounds: ", bounds)
 }
