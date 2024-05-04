2613c50170e9d3449503c78924dbf7c9bfe8e534
Igor B <84845632+Ig0rFB@users.noreply.github.com>
2024-05-04 02:15:53 +0100

    boop

--- src/imgResize/imgResize.go
+++ src/imgResize/imgResize.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image"
+	"image/color"
 	_ "image/jpeg"
 	"log"
 	"os"
@@ -14,11 +15,39 @@ func main(){
 	}
 defer file.Close()
 
-decodedImg, _, err := image.Decode(file)
+img, format, err := image.Decode(file)
 if err != nil {
 	log.Fatalln("Image decode error, check file format")
 }
 
-bounds := decodedImg.Bounds()
-log.Print("Image Bounds: ", bounds)
+averageColour := averagePixelValue(img)
+log.Printf("Average PV: R: %d, G: %d, B: %d, A: %d\n",
+averageColour.R, averageColour.G, averageColour.B, averageColour.A)
+ log.Printf("Decoded image format: %s", format)
+}
+
+func averagePixelValue(img image.Image) color.RGBA {
+	bounds := img.Bounds()
+	log.Printf("Image Bounds: %s", bounds)
+	var rTotal, gTotal, bTotal, aTotal uint64
+	var count uint64
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := img.At(x, y). RGBA()
+
+			rTotal += uint64(r >> 8)
+			gTotal += uint64(g >> 8)
+			bTotal += uint64(b >> 8)
+			aTotal += uint64(a >> 8)
+			count++
+		}
+	}
+
+	return color.RGBA{
+		R: uint8(rTotal / count),
+		G: uint8(gTotal / count),
+		B: uint8(bTotal / count),
+		A: uint8(aTotal / count),
+	}
 }
